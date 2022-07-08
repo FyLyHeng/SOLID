830f47b39d3f79bc580e3c9dd6e53f9325319673
fylyheng.phally <fylyheng.phally@gmail.com>
2022-07-08 21:53:13 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/SOLID/SolidApplication.kt
@@ -0,0 +1,13 @@
+package com.example.SOLID
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SolidApplication
+
+fun main(args: Array<String>) {
+	runApplication<SolidApplication>(*args)
+	println("am back so miss u java!")
+
+}
